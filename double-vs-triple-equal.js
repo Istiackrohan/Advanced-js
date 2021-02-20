5536699a73b404c918f1fbaf9e63257582cb3b94
Istiackrohan <istiackrohan@gmail.com>
2021-02-20 14:25:10 +0600

    map filter r find

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,13 @@
+const first = `2`;
+const second = 2;
+
+// false == 0;
+// true == 1;
+// 2 == "2";
+
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
