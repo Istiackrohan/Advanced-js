2456f6409cbd1f9230eb503666a7bc9855e5cf84
Istiackrohan <istiackrohan@gmail.com>
2021-02-20 14:48:24 +0600

    map filter r find clear

--- object-property.js
+++ object-property.js
@@ -4,16 +4,18 @@ const students = [
     {id: 41, name: 'Moyouri'},
     {id: 71, name: 'DeepJol'}
 ];
-let empty = [];
-for (let i = 0; i < students.length; i++) {
-    const element = students[i];
-    const names = element.name;
-    empty.push(names);
-}
-console.log(empty);
+
+// let empty = [];
+// for (let i = 0; i < students.length; i++) {
+//     const element = students[i];
+//     const names = element.name;
+//     empty.push(names);
+// }
+// console.log(empty);
+
 // const names = students.map( s => s.name);
 // const ids = students.map(s => s.id);
 // const bigger = students.filter( s => s.id>40);
 // const biggerOne = students.find( s => s.id>40);
 
-// console.log(biggerOne);
+console.log(students);
